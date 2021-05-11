fb1b0f3363b95d84abd2a8d67021ce67796953f4
Krzysztof Ograbek <krzysztof.ograbek@gmail.com>
2021-05-11 08:10:21 +0200

    feat: store articles using pickle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import pickle
 
 from scraper import get_site_text
 
@@ -13,18 +14,36 @@ def parse_arguments():
     return args
 
 
+def store_articles(articles):
+    with open('pickle_files/articles', 'ab') as f:
+        pickle.dump(articles, f)
+
+
+def load_articles():
+    with open('pickle_files/articles', 'rb') as f:
+        articles = pickle.load(f)
+    return articles
+
+def get_articles_from_www(args):
+    store_articles(get_site_text(args.date, args.days))
+
+    articles = load_articles()
+    print("from pickle", len(articles))
+    
+    # dbfile.close()
+    # sentences = []
+    # for article in articles:
+    #     starts = []
+    #     print(" - " *20)
+    #     print(article[0])
+
+
 def main():
     args = parse_arguments()
     get_articles_from_www(args)
 
 
-def get_articles_from_www(args):
-    articles = get_site_text(args.date, args.days)
-    sentences = []
-    for article in articles:
-        starts = []
-        print(" - " *20)
-        print(article[0])
+
 
 
 if __name__ == '__main__':
